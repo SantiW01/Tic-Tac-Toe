d2dbf10f177cf64d484d550f8356324f2a927f99
SantiW01 <sweihmuller01@gmail.com>
2023-03-29 17:46:45 -0300

    feat: second player can choose a block and an O appear

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const Game = (() => {
       player1Turn.setAttribute("class", "player1");
       block.appendChild(player1Turn);
       playerTurn = false;
+    } else if (!playerTurn) {
+      const player2Turn = document.createElement("p");
+      player2Turn.textContent = "O";
+      player2Turn.setAttribute("class", "player2");
+      block.appendChild(player2Turn);
+      playerTurn = true;
     }
   };
   const clickBlock = () => {
