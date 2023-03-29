54c95761760490af4889e72459b33a181fcbc5a2
SantiW01 <sweihmuller01@gmail.com>
2023-03-29 17:35:25 -0300

    feat: appear X in block clicked by user

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const Game = (() => {
+  const play = (e) => {
+    const block = e.target;
+    if (playerTurn) {
+      const player1Turn = document.createElement("p");
+      markX.textContent = "X";
+      markX.setAttribute("class", "player1");
+      block.appendChild(markX);
+      playerTurn = false;
+    }
+  };
   const clickBlock = () => {
     let blockSelected = document.querySelectorAll(".main");
     blockSelected.forEach((block) => {
